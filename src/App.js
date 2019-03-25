619e38f14096a9372f51cae713df31dd76afbb7a
Enida <enidaguja@hotmail.com>
2019-03-25 11:00:08 +0100

    First Commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import styles from './App.css';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className={styles.App}>
+        <p>Burger Builder</p>
+      </div>
+    );
+  }
+}
+
+export default App;
